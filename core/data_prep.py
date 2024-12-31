64875308539aba9e1a96434b5cce1f720f3974a2
KszyKszy0 <146132130+KszyKszy0@users.noreply.github.com>
2024-12-31 11:32:58 +0100

    data fix

--- core/data_prep.py
+++ core/data_prep.py
@@ -6,7 +6,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import os
 
+'''
+
+        Format danych
+        self.data[a][b]
+
+        self.data[a][0][0-1]
+        self.data[a][1]
+
+        [a][0][0] - input, [a][0][1] - target
+
+        [a][1] - tekst
+
+'''
 # Funkcja do wczytania i sparsowania pliku SVG
 def parse_svg(file_path):
     """
@@ -46,40 +60,65 @@ def parse_svg(file_path):
     2- wymiar punkt czyli np. [2][4] - oznacza 3 plik 5 punkt
 '''
 class HandwritingDataset(Dataset):
-    def __init__(self, svg_files):
+    def __init__(self, svg_files, text_file):
         """
         Dataset dla uczenia modelu na danych ręcznego pisma w formacie SVG.
+        Args:
+            svg_files (list): Lista ścieżek do plików SVG.
+            text_file (str): Ścieżka do pliku tekstowego zawierającego teksty (jedna linia na plik SVG).
         """
         self.data = []  # Lista sekwencji (każda sekwencja to lista punktów)
-        for file in svg_files:
+        self.texts = []  # Lista tekstów odpowiadających danym
+
+        # Wczytanie tekstów z pliku
+        with open(text_file, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            self.texts = [line.strip() for line in lines]
+
+        # Sprawdzenie, czy liczba tekstów zgadza się z liczbą plików SVG
+        if len(self.texts) != len(svg_files):
+            raise ValueError("Liczba tekstów w pliku nie zgadza się z liczbą plików SVG.")
+
+        # Wczytanie danych z plików SVG
+        for file, text in zip(svg_files, self.texts):
             # Parsowanie pliku SVG na punkty
             polylines = parse_svg(file)
-            self.data.append(polylines)  # Dodanie całej sekwencji z pliku
+            # Dodanie całej sekwencji z pliku oraz odpowiadającego tekstu
+            self.data.append((polylines, text))
 
         # Normalizacja danych
         self.normalize_data()
-        # print(self.data)
 
 
     def normalize_data(self):
         """
-        Normalizacja współrzędnych x, y. Flaga (0-1) nie jest normalizowana.
+        Normalizacja współrzędnych x, y w danych SVG. Flaga (0-1) pozostaje bez zmian.
+        Tekst (druga kolumna krotek) nie jest modyfikowany.
         """
-        all_points = np.concatenate([np.array(seq)[:, :2] for seq in self.data], axis=0)  # Ekstrakcja x, y
+        # Ekstrakcja wszystkich punktów x, y do jednego arraya
+        all_points = np.concatenate(
+            [np.array(seq[0])[:, :2] for seq in self.data], axis=0  # seq[0] to `polylines`
+        )
         self.mean = np.mean(all_points, axis=0)
         self.std = np.std(all_points, axis=0)
 
+        # Zapis średniej i odchylenia standardowego do pliku
         with open('norm.txt', 'w') as file:
-            np.savetxt('norm.txt', np.column_stack((self.mean, self.std)))
+            np.savetxt(file, np.column_stack((self.mean, self.std)))
 
-        # Normalizacja x, y; flaga (0-1) pozostaje bez zmian
+        # Normalizacja x, y w `polylines`
         for i in range(len(self.data)):
-            self.data[i] = [
-                [(point[0] - self.mean[0]) / self.std[0],
-                 (point[1] - self.mean[1]) / self.std[1],
-                 point[2]]  # Flaga niezmieniona
-                for point in self.data[i]
+            polylines, text = self.data[i]
+            normalized_polylines = [
+                [
+                    (point[0] - self.mean[0]) / self.std[0],  # Normalizacja x
+                    (point[1] - self.mean[1]) / self.std[1],  # Normalizacja y
+                    point[2],  # Flaga bez zmian
+                ]
+                for point in polylines
             ]
+            # Aktualizacja znormalizowanych danych, pozostawiając tekst bez zmian
+            self.data[i] = (normalized_polylines, text)
 
     def __len__(self):
         """
@@ -91,17 +130,17 @@ class HandwritingDataset(Dataset):
         """
         Zwraca sekwencję wejściową i docelową dla danego indeksu pliku.
         """
-        polyline = self.data[idx]
+        polyline = self.data[idx][0]
         input_seq = torch.tensor(polyline[:-1], dtype=torch.float32)  # Wszystko oprócz ostatniego punktu
         target_seq = torch.tensor(polyline[1:], dtype=torch.float32)  # Wszystko oprócz pierwszego punktu
-        return input_seq, target_seq
+        return input_seq, target_seq, self.data[idx][1]
 
 
-
-# svg_files = ['output/00001.svg','output/00002.svg']  # Podmień na rzeczywiste ścieżki
-# dataset = HandwritingDataset(svg_files)
+# folder_path = "output"
+# svg_files = ["output/" + file for file in os.listdir(folder_path) if file.endswith('.svg')]
+# dataset = HandwritingDataset(svg_files, "output/files.txt")
 # dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 
 
-# for input_seq, target_seq in dataloader:
-#     print(input_seq, target_seq)
+# for input_seq, target_seq, text in dataloader:
+#     print(text)
